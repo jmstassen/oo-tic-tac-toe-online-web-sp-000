7770d0f2d7c36680febb0319c0ee96693bec2eaa
John Stassen <john.stassen@gmail.com>
2020-08-26 02:29:33 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,89 @@
+require 'pry'
+class TicTacToe
+
+  WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+
+  def initialize
+    @board = Array.new(9, " ")
+  end
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(user_input)
+    index = user_input.to_i - 1
+  end
+
+  def move(index, token = "X")
+    @board[index] = token
+  end
+
+  def position_taken?(index)
+    if @board[index] == "X" || @board[index] == "O"
+      true
+    else
+      false
+    end
+  end
+
+  def valid_move?(position)
+    if position.between?(0,8) == false
+      false
+    elsif @board[position] == "X" || @board[position] == "O"
+      false
+    else
+      true
+    end
+  end
+
+
+  def turn
+  end
+
+  def turn_count
+    @turn_count = 0
+    @board.each do |position|
+      if position == "X" || position == "O"
+        @turn_count += 1
+      else
+      end
+    end
+    @turn_count
+  end
+
+
+  def current_player
+    @board.turn_count % 2 == 0
+      "X"
+    else
+      "O"
+    end
+  end
+
+  def won?
+  end
+
+  def full?
+  end
+
+
+  def draw?
+  end
+
+
+  def over?
+  end
+
+
+  def winner?
+  end
+
+
+  def play
+end
+end
