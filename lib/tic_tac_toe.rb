02fb753f19bc289bbbfabfd244791ad69b61cf5d
John Stassen <john.stassen@gmail.com>
2020-08-26 03:19:43 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -78,9 +78,9 @@ class TicTacToe
   def won?
     WIN_COMBINATIONS.each do |combo|
       if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X" || @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
-        true
+        return true
       else
-        false
+        return false
       end
     end
   end
